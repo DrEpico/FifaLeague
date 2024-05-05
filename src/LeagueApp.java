3bda83a24d88a61765c2f41da2c3c36536bdedb8
Ali <adadashipour@gmail.com>
2024-05-05 20:51:04 +0100

    Tesseract OCR can be used now

--- src/LeagueApp.java
+++ src/LeagueApp.java
@@ -52,7 +52,17 @@ public class LeagueApp {
         Manager manager = new Manager();
         manager.addManager();
     }
-
-
-
 }
+
+
+//public static void main(String[] args) throws SQLException {
+//    Tesseract tesseract = new Tesseract();
+//    try {
+//        tesseract.setDatapath("C:\\Users\\adada\\Downloads\\Tess4J-3.4.8-src\\Tess4J\\tessdata");
+//        // the path of your .jpg, .png or .gif file
+//        String text = tesseract.doOCR(new File("C:\\Users\\adada\\Documents\\ShareX\\Screenshots\\2024-01\\FC24_byUzkGEoE2.png"));
+//        System.out.println(text);
+//    } catch (TesseractException e) {
+//        e.printStackTrace();
+//    }
+//}
